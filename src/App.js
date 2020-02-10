3a4a108e715e6749b3a5d41515f995ff17ef50ad
Pierre B <pierre.barbin59@gmail.com>
2020-02-10 14:01:14 +0100

    quete2

--- src/App.js
+++ src/App.js
@@ -8,16 +8,19 @@ class App extends Component {
     state = {
         persons: [
             {
+                id: 1,
                 name: "JBoss",
                 avatar: "https://radiomedecinedouce.com/attachments/images/broadcast/41063/medium.png",
                 online: true
             },
             {
+                id: 2,
                 name: "Pierre",
                 avatar: "https://pbs.twimg.com/profile_images/595659609349718016/QpJAamaV_400x400.jpg",
                 online: false
             },
             {
+                id: 3,
                 name: "Jean",
                 avatar: "https://is2-ssl.mzstatic.com/image/thumb/Purple113/v4/c5/c9/15/c5c915bb-c247-72c1-05cb-78b574563c51/source/256x256bb.jpg",
                 online: true
@@ -29,7 +32,7 @@ class App extends Component {
         return (
             <div className="App">
                 {this.state.persons.map((person) => {
-                    return <Contact person={person} />
+                    return <Contact key={person.id} person={person} />
                 })}
             </div>
         );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,13 +2,23 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+
+    isOnline(){
+        if(this.props.person.online){
+            return <p>Online<span className="status-online"></span></p>;
+        }
+        return <p>Offline<span className="status-offline"></span></p>;
+    }
+
     render() {
-        let {name, online, avatar} = this.props.person;
+        let {name, avatar} = this.props.person;
         return (
-            <div className="person-item">
-                <h4>{ name }</h4>
-                <p>{ online ? 'Online' : 'Offline' }</p>
+            <div className="Contact">
                 <img className="avatar" src={avatar} alt={avatar}/>
+                <h4 className="name">{ name }</h4>
+                <div className="status">
+                    <div className="status-text">{ this.isOnline() }</div>
+                </div>
             </div>
         );
     }
